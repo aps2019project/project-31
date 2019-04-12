0c88ebdecffe81718df32a520d310641fe22228e
Gnomy17 <45305878+Gnomy17@users.noreply.github.com>
2019-04-12 22:37:41 +0430

    Initial commit

---
+++ src/Functions.java
@@ -0,0 +1,16 @@
+public class Functions {
+    public static final String STUN = "stun target for";
+    public static final String ACCUMULATING_ATTACK = "accumulate previous attacks on target";
+    public static final String DISARM = "disarm target for";
+    public static final String APPLY_POISON = "poison target for";
+    public static final String DEAL_DAMAGE = "deal damage";
+    public static final String IGNORE_HOLYBUFF = "ignore holybuff";
+    public static final String APPLY_CONDITION = "apply condition";
+    public static final String APPLY_BUFF = "apply buff";
+    public static final String REVERT_DISARM = "revert disarm";
+    public static final String REMOVE_POISON = "remove poison";
+    public static final String REMOVE_DEBUFFS = "remove debuffs";
+    public static final String IGNORE_ATTACK = "ignore attack";
+    public static final String IGNORE_LESSER_ATTACK = "ignore lesser attacks";
+    public static final String REMOVE_BENEFICIALS = "remove beneficial effects";
+}
