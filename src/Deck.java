daab9675cd59f7a77f6d386b2302b80a1f65a4ea
sina0k <sina0ahmadkhani@gmail.com>
2019-04-14 03:16:10 +0430

    Deck created, methods added

---
+++ src/Deck.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Deck {
+    private String deckName;
+    private ArrayList<Card> cards;
+    private Hero hero;
+
+    public String getDeckName() {
+        return deckName;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void shuffle() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            int first = random.nextInt(cards.size());
+            int second = random.nextInt(cards.size());
+            Collections.swap(cards,first,second);
+        }
+    }
+    public Deck getDuplicateDeck(){
+        return null; // ???
+    }
+}
