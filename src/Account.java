e2de71d3a3fe1f85f672ed87de26b5306d30d147
sina0k <sina0ahmadkhani@gmail.com>
2019-04-14 12:21:18 +0430

    account fixed,matchHistory created

--- src/Account.java
+++ src/Account.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Account {
+    private static ArrayList<Account> allAccounts = new ArrayList<>();
+    private static Account mainAccount;
     private ArrayList<Card> collection;
     private int daric;
     private ArrayList<Deck> decks;
     private Deck theMainDeck;
-    private ArrayList<Match> matchHistories;
+    private String username;
+    private String password;
+    private ArrayList<MatchHistory> matchHistories;
+    private int[] winLoseDraw = new int[3];
 
     public ArrayList<Card> getCollection() {
         return collection;
@@ -23,7 +29,64 @@ public class Account {
         return theMainDeck;
     }
 
-    public ArrayList<Match> getMatchHistories() {
+    public ArrayList<MatchHistory> getMatchHistories() {
         return matchHistories;
+    }
+
+    public boolean checkLogin(String username, String password) {
+        Account account = findTheAccount(username);
+        if (account != null && account.password.equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    private Account findTheAccount(String username) {
+        for (Account account : allAccounts) {
+            if (account.username.equals(username)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public static void changeMainAccount(String username)
+
+    public static void addAccount(Account account) {
+        allAccounts.add(account);
+    }
+
+    public void showAccounts() {
+        //WTF is there to show ???
+    }
+
+    public static void showLeaderBoard() {
+        sortAllAccounts();
+        for (int i = 0; i <allAccounts.size() ; i++) {
+            System.out.println((i+1) +" - UserName: "+ allAccounts.get(i).username +" -Wins: " +
+                    allAccounts.get(i).winLoseDraw[0]);
+        }
+
+    }
+    private static void sortAllAccounts(){
+        for (int i = 0; i <allAccounts.size() ; i++) {
+            int theBest=i;
+            for (int j = i+1; j <allAccounts.size() ; j++) {
+                if(allAccounts.get(i).winLoseDraw[0]<allAccounts.get(j).winLoseDraw[0]){
+                    theBest=j;
+                }
+            }
+            if(theBest!=i){
+                Collections.swap(allAccounts,theBest,i);
+            }
+        }
+    }
+
+    public static ArrayList<Account> getAllAccounts() {
+        return allAccounts;
+    }
+
+    public static Account getMainAccount() {
+        return mainAccount;
     }
 }
