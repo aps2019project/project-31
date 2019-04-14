71ab27a550f3cf5e6221a025b9edabb1601884c5
Gnomy17 <gnomy@ce.sharif.edu>
2019-04-14 20:32:12 +0430

    Hero and Attack Type added

---
+++ src/Hero.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class Hero extends Card {
+    protected AttackType attackType;
+    protected int attack;
+    protected int health;
+    protected HeroSpell heroSpell;
+
+    public Hero(CardInfo cardInfo,
+                int price,
+                int manaCost,
+                String cardText,
+                ArrayList<Function> functions,
+                Account account,
+                AttackType attackType,
+                int attack, int health,
+                HeroSpell heroSpell) {
+        super(cardInfo, price,
+                manaCost, cardText,
+                functions, account);
+        this.attackType = attackType;
+        this.attack = attack;
+        this.health = health;
+        this.heroSpell = heroSpell;
+    }
+
+    public void show() {
+        System.out.println(" Name: "
+                + getName()
+                + " - AP : "
+                + attack
+                + " - HP : "
+                + health
+                + " class : "
+                + attackType
+                + " Special power : "
+                + heroSpell.cardText);
+    }
+
+    public void setAttackType(AttackType attackType) {
+        this.attackType = attackType;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setHeroSpell(HeroSpell heroSpell) {
+        this.heroSpell = heroSpell;
+    }
+
+    public AttackType getAttackType() {
+        return attackType;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public HeroSpell getHeroSpell() {
+        return heroSpell;
+    }
+}
