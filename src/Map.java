f8384935523820283d4f5d3fa76bf61dde8b2428
sina0k <sina0ahmadkhani@gmail.com>
2019-04-15 10:17:57 +0430

    getters added

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,5 @@
 public class Map {
-    private final int maxMoveRange = 2;
+    private static final int maxMoveRange = 2;
     private static Cell[][] map = new Cell[5][8];
 
     public Cell getCell(int x, int y) {
@@ -9,9 +9,18 @@ public class Map {
         return null;
     }
 
-    public int getDistance(Cell cell1, Cell cell2) {
+    public static int getDistance(Cell cell1, Cell cell2) {
         return Math.abs(cell1.getxCoordinate() - cell2.getxCoordinate()) +
                 Math.abs(cell1.getyCoordinate() - cell2.getyCoordinate());
+    }
+
+    public static int getMaxMoveRange() {
+        return maxMoveRange;
+    }
+
+    public static int getDistance(Cell cell1, int x, int y){
+        return Math.abs(cell1.getxCoordinate() - x) +
+                Math.abs(cell1.getyCoordinate() - y);
     }
 
     public void move(int x, int y, Card card) {
