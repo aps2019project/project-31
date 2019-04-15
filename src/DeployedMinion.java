f5c9364fffe57555ff11e41f22c1af00f83ee736
sina0k <sina0ahmadkhani@gmail.com>
2019-04-15 10:35:18 +0430

    Classes methods added

--- src/DeployedMinion.java
+++ src/DeployedMinion.java
@@ -1,8 +1,22 @@
 import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
 
 public class DeployedMinion extends Minion {
-    public DeployedMinion(CardInfo cardInfo, int price, int manaCost, String cardText,
-                          ArrayList<Function> functions, Account account) {
-        super(cardInfo, price, manaCost, cardText, functions, account);
+    private ArrayList<Condition> conditions;
+    private Cell cell;
+    private int currentHealth;
+    private int curretAttack;
+    private ArrayList<Buuf> buffs;
+
+    public DeployedMinion(CardInfo cardInfo, int price, int manaCost, String cardText, ArrayList<Function> functions,
+                          Account account, String functionTime, int attackRange, String attackType, int attack,
+                          int health, ArrayList<Condition> conditions, Cell cell, int currentHealth,
+                          int curretAttack, ArrayList<Buuf> buffs) {
+        super(cardInfo, price, manaCost, cardText, functions, account, functionTime, attackRange, attackType, attack, health);
+        this.conditions = conditions;
+        this.cell = cell;
+        this.currentHealth = currentHealth;
+        this.curretAttack = curretAttack;
+        this.buffs = buffs;
     }
 }
--- src/Minion.java
+++ src/Minion.java
@@ -1,9 +1,40 @@
 import java.util.ArrayList;
 
 public class Minion extends Card {
+    protected String functionTime;
+    protected int attackRange;
+    protected String attackType;
+    private int attack;
+    private int health;
+
     public Minion(CardInfo cardInfo, int price, int manaCost, String cardText, ArrayList<Function> functions,
-                  Account account) {
+                  Account account, String functionTime, int attackRange, String attackType, int attack, int health) {
         super(cardInfo, price, manaCost, cardText, functions, account);
+        this.functionTime = functionTime;
+        this.attackRange = attackRange;
+        this.attackType = attackType;
+        this.attack = attack;
+        this.health = health;
+    }
+
+    public String getFunctionTime() {
+        return functionTime;
+    }
+
+    public int getAttackRange() {
+        return attackRange;
+    }
+
+    public String getAttackType() {
+        return attackType;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getHealth() {
+        return health;
     }
 
     @Override
