c053c8860b206b0b1a67212d61eb4b247d8415d1
sina0k <sina0ahmadkhani@gmail.com>
2019-04-15 09:56:04 +0430

    deployedHero added

--- src/DeployedHero.java
+++ src/DeployedHero.java
@@ -1,12 +1,24 @@
 import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
 
-public class DeployedHero {
+public class DeployedHero extends Hero {
     private ArrayList<Condition> conditions;
     private Cell cell;
     private int currentHealth;
     private int currentAttack;
     private ArrayList<Buff> buffs;
+
+    public DeployedHero(
+            CardInfo cardInfo, int price, int manaCost, String cardText, ArrayList<Function> functions, Account account,
+            AttackType attackType, int attack, int health, HeroSpell heroSpell, ArrayList<Condition> conditions
+            , Cell cell, int currentHealth, int currentAttack, ArrayList<Buff> buffs) {
+        super(cardInfo, price, manaCost, cardText, functions, account, attackType, attack, health, heroSpell);
+        this.conditions = conditions;
+        this.cell = cell;
+        this.currentHealth = currentHealth;
+        this.currentAttack = currentAttack;
+        this.buffs = buffs;
+    }
 
     public ArrayList<Condition> getConditions() {
         return conditions;
@@ -27,7 +39,8 @@ public class DeployedHero {
     public ArrayList<Buff> getBuffs() {
         return buffs;
     }
-    public void show(){
+
+    public void show() {
 
     }
 }
