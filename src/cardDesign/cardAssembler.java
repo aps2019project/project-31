1e691f06d5642a1f7ef23394c4aad1ed07993790
Gnomy17 <gnomy@ce.sharif.edu>
2019-04-29 14:19:20 +0430

    Card assembler progress

--- src/cardDesign/cardAssembler.java
+++ src/cardDesign/cardAssembler.java
@@ -1,6 +1,21 @@
 package cardDesign;
 
 
+import model.FunctionStrings;
+
+import java.util.Scanner;
+
 public class cardAssembler {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select desired function with number");
+        for (String function: FunctionStrings.allFunctionStrings()){
+            System.out.println((FunctionStrings.allFunctionStrings().indexOf(function) + 1) +
+                    ". " + function);
+        }
+        StringBuilder functionToAdd = new StringBuilder();
+        functionToAdd.append(FunctionStrings.allFunctionStrings().get(scanner.nextInt() - 1));
+
+    }
 }
