6813c16b1de5df8a7cc5209e6d761048a8dd9668
Gnomy17 <gnomy@ce.sharif.edu>
2019-05-07 22:19:12 +0430

    Custom decks

--- src/model/SinglePlayer.java
+++ src/model/SinglePlayer.java
@@ -1,16 +1,40 @@
 package model;
 
+import com.gilecode.yagson.YaGson;
+import com.gilecode.yagson.YaGsonBuilder;
 import constants.GameMode;
 import controller.Menu;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class SinglePlayer extends BattleManager {
 
     private static Ai aiPlayer = null;
     private static Account AIAccount = null;
+    private static ArrayList<Deck> customGameDecks = new ArrayList<>();
+
 
     public SinglePlayer(Player player1, Player player2, int maxNumberOfFlags, int maxTurnsOfHavingFlag, GameMode gameMode) {
         super(player1, player2, maxNumberOfFlags, maxTurnsOfHavingFlag, gameMode);
 
+    }
+
+    public static void loadCustomDecks(){
+        YaGson yaGson = new YaGsonBuilder().create();
+        String path = System.getProperty("user.dir") + "/Sources/StoryDecks/CustomDecks.txt";
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
+            String line = bufferedReader.readLine();
+            customGameDecks.add(yaGson.fromJson(line,Deck.class));
+        }catch (IOException e){
+
+        }
+    }
+
+    public static void addCustomDeck(Deck deck){
+        customGameDecks.add(deck);
     }
 
     public static void makeAIAccount(Deck deck){
