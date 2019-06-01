cd8fa76b284c3322b413d9b44760a18dfdd5da75
Gnomy17 <gnomy@ce.sharif.edu>
2019-06-01 18:54:19 +0430

    Login page progress

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -11,11 +12,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginPageController implements Initializable {
+    @FXML
     public HBox mainContainer;
+    @FXML
     public TextField usernameTF;
+    @FXML
     public PasswordField passwordField;
+    @FXML
     public Button loginButton;
+    @FXML
     public ImageView middlePicture;
+    @FXML
+    private TextField signupUsername;
+    @FXML
+    private PasswordField signupPassword;
+    @FXML
+    private PasswordField repassword;
+    @FXML
+    private Button signupButton;
+    @FXML
+    private Button exitButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
