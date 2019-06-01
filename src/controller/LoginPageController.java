bd8fcae7c3585a2362fe064e3ec89bc3a97cca05
Gnomy17 <gnomy@ce.sharif.edu>
2019-06-01 16:27:33 +0430

    okay

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -4,7 +4,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
