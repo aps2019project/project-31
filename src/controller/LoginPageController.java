5625a73fccb0b7a73ce72c381525fa32352d5fda
sina0k <sina0ahmadkhani@gmail.com>
2019-06-01 15:50:06 +0430

    login page design progress

---
+++ src/controller/LoginPageController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginPageController implements Initializable {
+    public HBox mainContainer;
+    public TextField usernameTF;
+    public PasswordField passwordField;
+    public Button loginButton;
+    public ImageView middlePicture;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
