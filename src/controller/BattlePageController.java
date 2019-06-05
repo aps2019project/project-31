e0b5ee01d969bf677bc71160aeadb72616a0b219
sina0k <sina0ahmadkhani@gmail.com>
2019-06-06 01:18:16 +0430

    BattlePage,better look

--- src/controller/BattlePageController.java
+++ src/controller/BattlePageController.java
@@ -1,12 +1,40 @@
 package controller;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BattlePageController implements Initializable {
 
+
+    public Label player1Health;
+    public Label player1Username;
+    public Label player1GeneralCoolDown;
+    public Label player2Health;
+    public Label player2Username;
+    public Label OpponentHand;
+    public Label opponentMana;
+    public Label player2GeneralCoolDown;
+    public Label DeckSize;
+    public ImageView nextCardField;
+    public ImageView mana1;
+    public ImageView mana2;
+    public ImageView mana3;
+    public ImageView mana4;
+    public ImageView mana5;
+    public ImageView mana6;
+    public ImageView mana7;
+    public ImageView mana8;
+    public ImageView mana9;
+    public Label manaCost1;
+    public Label manaCost2;
+    public Label manaCost3;
+    public Label manaCost4;
+    public Label manaCost5;
+    public Label manaCost6;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
